db219828eeae8df5ce852aaad76bb90b740f22d3
Vladimir Kharapaev <vkharapaev@gmail.com>
2021-06-06 22:57:04 +0500

    initial

---
+++ MaterialDesignApp/app/src/main/java/com/headmostlab/materialdesignapp/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.headmostlab.materialdesignapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.headmostlab.materialdesignapp.R
+
+class MainActivity : AppCompatActivity(R.layout.main_activity)
---
+++ MaterialDesignApp/app/src/main/java/com/headmostlab/materialdesignapp/ui/main/PictureOfTheDayFragment.kt
@@ -0,0 +1,6 @@
+package com.headmostlab.materialdesignapp.ui.main
+
+import androidx.fragment.app.Fragment
+import com.headmostlab.materialdesignapp.R
+
+class PictureOfTheDayFragment : Fragment(R.layout.picture_of_the_day_fragment)
